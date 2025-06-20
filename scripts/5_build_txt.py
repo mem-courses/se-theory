fa6779a3ee0ddd4720f3b1212f3915442dde883e
memset0 <memset0@outlook.com>
2025-06-20 23:58:49 +0800

    feat: 增加导出成txt的功能

--- scripts/5_build_txt.py
+++ scripts/5_build_txt.py
@@ -25,24 +25,23 @@ output_dir = os.path.join(dirname, "..", "build")
 def parse_statement(statement, options, answer=[], index=None):
     statement = statement.replace('"', '\\"')
     if index is not None:
-        statement += f'" + index({index}) + "'
+        statement += f' #{index}'
     if len(answer) == 0:
-        result = '#statement([($quad$) ] + "' + statement + '")'
+        result = '(  ) ' + statement
     else:
-        result = f'#text([({"".join(answer)}) ] + "' + statement + '")'
+        result = f'({"".join(answer)}) ' + statement
     if options is not None:
-        result += '\n\n#options(\n'
+        result += '\n'
         for char, option in zip('ABCD', options):
             if char in answer:
-                result += f'  "{char}.", correct-option[{option}],\n'
+                result += f'- {char}. {option}\n'
             else:
-                result += f'  "{char}.", [{option}],\n'
-        result += ')'
-    return result
+                result += f'- {char}. {option}\n'
+    return result + '\n\n'
 
 
 def parse_explaination(explaination):
-    return '#explaination[' + explaination + ']'
+    return explaination + '\n\n'
 
 
 tasks = [
@@ -51,10 +50,7 @@ tasks = [
     },
     {
         'fields': ['en'],
-    },
-    {
-        'fields': ['en', 'cn'],
-    },
+    }
 ]
 for task in tasks:
     task['name'] = f'软工题库mem整理版_v{VERSION}_{'+'.join(task["fields"])}'
@@ -74,28 +70,29 @@ for chapter in os.listdir(source_dir):
         if prob['type'] == 'TF':
             en = parse_statement(prefix + prob['topic'], None, index=index)
             cn = parse_statement(prefix + prob['topic_cn'], None, index=index)
-            en_ans = parse_statement(prob['topic'], None, prob['answer'])
-            cn_ans = parse_statement(prob['topic_cn'], None, prob['answer'])
+            en_ans = parse_statement(prefix + prob['topic'], None, prob['answer'])
+            cn_ans = parse_statement(prefix + prob['topic_cn'], None, prob['answer'])
         else:
             en = parse_statement(prefix + prob['topic'], prob['options'], index=index)
             cn = parse_statement(prefix + prob['topic_cn'], prob['options_cn'], index=index)
-            en_ans = parse_statement(prob['topic'], prob['options'], prob['answer'])
-            cn_ans = parse_statement(prob['topic_cn'], prob['options_cn'], prob['answer'])
+            en_ans = parse_statement(prefix + prob['topic'], prob['options'], prob['answer'])
+            cn_ans = parse_statement(prefix + prob['topic_cn'], prob['options_cn'], prob['answer'])
             # print(en_ans)
 
         with open(os.path.join(explaination_dir, chapter, problem + '.md'), 'r', encoding='utf-8') as f:
             explaination = f.read()
         expl = parse_explaination(explaination)
-        expl_ans = parse_explaination(explaination + '][' + cn_ans)
+        expl_ans = parse_explaination(explaination + '\n\n' + cn_ans)
 
         for task in tasks:
             for field in task['fields']:
                 task['data'].append(eval(f'{field}'))
-        task['data'].append('#spacing')
+        task['data'].append('\n')
 
 for task in tasks:
-    filename = os.path.join(output_dir, f'{task["name"]}.typ')
+    filename = os.path.join(output_dir, f'{task["name"]}.txt')
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(TEMPLATE_HEADER + '\n\n\n')
-        f.write('\n\n'.join(task['data']))
-        f.write('\n\n#v(2em)\n\n#hint')
+        content = '\n\n'.join(task['data'])
+        for _ in range(10):
+            content = content.replace('\n\n\n', '\n\n')
+        f.write(content)
